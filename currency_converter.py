e8c291765224b02c8ed5952d6e28272c17bbd8bd
Ondřej Divina <Ondra.Divina@seznam.cz>
2020-01-13 22:41:58 +0100

    Getting exchange rates
    - API request GET from exchangeratesapi.io
    -Corrected option of no output currency

--- currency_converter.py
+++ currency_converter.py
@@ -1,5 +1,7 @@
 import argparse as ap
-
+import requests
+import json
+import pprint # for testing purposes
 
 def add_parameters_needed(args_parser: ap.ArgumentParser):
 	"""
@@ -9,7 +11,7 @@ def add_parameters_needed(args_parser: ap.ArgumentParser):
 	"""
 	args_parser.add_argument("--amount", required=True, help="Amount of currency to convert")
 	args_parser.add_argument("--input_currency", required=True, help="Currency of the amount on input")
-	args_parser.add_argument("--output_currency", required=True, help="Currency of desired output")
+	args_parser.add_argument("--output_currency", required=False, help="Currency of desired output")
 	return args_parser
 
 
@@ -25,8 +27,33 @@ if __name__ == "__main__":
 	# Get dictionary of parameters and values
 	args = vars(argsParser.parse_args())
 
+	# Declare amount for future use (syntax checking)
+	amount = 0
+	try:
+		amount = float(args["amount"])
+	except ValueError as valE:
+		print("Argument --amount should be numeric (use . as decimal sign)")
+		exit(2)
+	iCurrency = args["input_currency"]
+	oCurrency = args["output_currency"]
 
-	print("End of script\n")
+	# Get rates from exchangeratesapi.io
+	curGet = requests.get(f"https://api.exchangeratesapi.io/latest?base={iCurrency}")
+	if curGet.status_code == 440:
+		print("Unable to retrieve exchange rates from server.")
+		exit(2)
+	elif curGet.status_code == 400:
+		print(curGet.text)
+		exit(2)
+	elif curGet.status_code != 200:
+		print("Unexpected error during obtaining exchange rates")
+		print(f"Code: {curGet.status_code} - {curGet.text}")
+
+	currencyRates = json.loads(curGet.text)["rates"]
+
+	if (oCurrency is not None) & (oCurrency not in currencyRates.keys()):
+		print(f"Unsupported output currency: {oCurrency}")
+		exit(2)
 
 
 
